cb39da3260502f6209f369a98b47870c96456879
S B RAGHAVENDRA <149289779+Raghusb@users.noreply.github.com>
2026-09-03 20:54:17 +0530

    encoder8_3.v
    Module: encoder8_3.v
    - RTL Design:
      * Behavioral 8:3 binary encoder using always @(*) combinational block.
      * Active-HIGH enable gating forcing y = 3'b000 when disabled.
      * Explicit case statement covering all 8 one-hot inputs (8'b0000_0001 to 8'b1000_0000).
      * Default case prevents inferencing memory latches on non-one-hot inputs.

---
+++ rtl/encoder8_3.v
@@ -0,0 +1,23 @@
+module encoder8_3( input en, input [7:0] in, output reg [2:0] y 
+
+    );
+    
+    always@(*)
+    begin
+    if(!en)
+    y = 0;
+    else
+    case(in)
+    8'b0000_0001: y=3'b000;
+    8'b0000_0010: y=3'b001;
+    8'b0000_0100: y=3'b010;
+    8'b0000_1000: y=3'b011;
+    8'b0001_0000: y=3'b100;
+    8'b0010_0000: y=3'b101;
+    8'b0100_0000: y=3'b110;
+    8'b1000_0000: y=3'b111;
+    default: y=3'b0;
+    endcase
+    end
+    
+endmodule
